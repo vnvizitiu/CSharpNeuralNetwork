1d3b4d4b0c9e918081d1b16750edf6bb432edadd
Ghost <Ghost@GHOST-KOMPUTER>
2016-10-23 23:19:56 +0200

    optimisation

--- NeuralNetwork_WinForms/NeuralNetwork.cs
+++ NeuralNetwork_WinForms/NeuralNetwork.cs
@@ -14,51 +14,19 @@ namespace NeuralNetwork_WinForms
     public partial class Form1 : Form
     {
 
+        NeuralNetwork nn;
 
-        List<double[]> Points;
-        int inputsCount;
-        int outputsCount;
-        int totalLayersCount;
-        int perceptronsInHiddenLayer;
-        Random rnd;
 
-        Perceptron[][] p;
 
         public Form1()
         {
             InitializeComponent();
-            Points = new List<double[]>();
-            rnd = new Random();
 
-            inputsCount = 12;
-            outputsCount = 1;
-            totalLayersCount = 4;
-            perceptronsInHiddenLayer =5;
-
-            InitTopology(inputsCount, outputsCount, totalLayersCount, perceptronsInHiddenLayer, rnd);
+            nn= new NeuralNetwork(9, 1, 3, 5);
 
             pictureBoxResult.Refresh();
         }
 
-        public void InitTopology(int _inputs, int _outputs, int _totalLayersCount, int _perceptronsInHiddenLayer, Random _rnd)
-        {
-            p = new Perceptron[_totalLayersCount][];// 3 layers
-
-            p[0] = new Perceptron[inputsCount];//input layer
-            p[_totalLayersCount - 1] = new Perceptron[outputsCount];//output layer 
-
-            for (int i = 1; i < _totalLayersCount - 1; i++) //hidden layers
-                p[i] = new Perceptron[_perceptronsInHiddenLayer];
-
-
-            //initialize weight and other stuff
-            for (int layer = 0; layer < p.Length; layer++)
-                for (int perceptron = 0; perceptron < p[layer].Length; perceptron++)
-                    if (layer - 1 < 0)
-                        p[layer][perceptron] = new Perceptron(2,rnd);
-                    else
-                        p[layer][perceptron] = new Perceptron(p[layer - 1].Length,rnd);
-        }
 
 
         private void pictureBoxResult_Paint(object sender, PaintEventArgs e)
@@ -70,20 +38,20 @@ namespace NeuralNetwork_WinForms
                 for (int x = 0; x < 250; x++)
                     for (int y = 0; y < 250; y++)
                     {
-                        double[] matrix = new double[2];
-                        matrix[0] = ((((double)x) / 250) * 2) - 1;
-                        matrix[1] = ((((double)y) / 250) * 2) - 1;
+                        float[] matrix = new float[2];
+                        matrix[0] = ((((float)x) / 250) * 2) - 1;
+                        matrix[1] = ((((float)y) / 250) * 2) - 1;
 
-                        CalculateOutputs(matrix);
+                       
 
-                        int color = (int)((p[totalLayersCount - 1][0].Output+1) * 127);
+                        int color = (int)((nn.CalculateOutputs(matrix)+1) * 127);
 
                         pen = new Pen(Color.FromArgb(255, color, color, color), 1);
                         e.Graphics.DrawLine(pen, (float)x, (float)y, (float)x + 1, (float)y + 1);
                     }
 
-                if (Points != null && Points.Count > 0)
-                    foreach (double[] point in Points)
+                if (nn.InputWithOneOutputMatrix != null && nn.InputWithOneOutputMatrix.Count > 0)
+                    foreach (float[] point in nn.InputWithOneOutputMatrix)
                     {
                         if (point[2] == 1)
                             pen = new Pen(Color.Red, 1);
@@ -105,9 +73,9 @@ namespace NeuralNetwork_WinForms
         private void pictureBoxResult_MouseClick(object sender, MouseEventArgs e)
         {
 
-            double[] matrix = new double[3];
-            matrix[0] = ((((double)e.X) / 250)*2)-1;
-            matrix[1] = ((((double)e.Y) / 250) * 2) - 1;
+            float[] matrix = new float[3];
+            matrix[0] = ((((float)e.X) / 250)*2)-1;
+            matrix[1] = ((((float)e.Y) / 250) * 2) - 1;
 
             if (e.Button == MouseButtons.Left)
                 matrix[2] = 1;
@@ -116,86 +84,26 @@ namespace NeuralNetwork_WinForms
             else
                 matrix[2] = -1;
 
-
-            Points.Add(matrix);
+            nn.InputWithOneOutputMatrix.Add(matrix);
             pictureBoxResult.Refresh();
         }
 
 
-        private void CalculateOutputs(double[] _point)
-        {
-            for (int i = 0; i < p[0].Length; i++)
-                p[0][i].ShowMatrix(_point);//show data input layers
 
-            for (int layers = 1; layers < totalLayersCount; layers++)  // show data rest layers
-                for (int i = 0; i < p[layers].Length; i++)
-                {
-                    double[] matrix = new double[p[layers - 1].Length];
-                    for (int j = 0; j < p[layers - 1].Length; j++)
-                        matrix[j] = p[layers - 1][j].Output;
-
-                    p[layers][i].ShowMatrix(matrix);
-                }
-        }
 
         private void buttonLearn_Click(object sender, EventArgs e)
         {
-            //iterate all
-            for (int i = 0; i < 2000; i++)
-                foreach (double[] point in Points)
-                {
-                    CalculateOutputs(new double[] { point[0], point[1] });
-                    CalculateSigma(point[2]);
-                    UpgradeWeight(point);
-                }
-
+            nn.Learn();
             pictureBoxResult.Refresh();
         }
 
-        private void UpgradeWeight(double[] _point)
-        {
-            for (int layer = 0; layer < totalLayersCount; layer++)
-                for (int i = 0; i < p[layer].Length; i++)
-                {
-                    double input = 0;
-                    for (int j = 0; j < p[layer][i].weight.Length; j++)
-                    {
-                       
-                        if (layer == 0)
-                            input = _point[j];///pomylonailosc wejsc ziloscia neuronow wejsciowych
-                        else
-                            input = p[layer - 1][j].Output;
-                        double result = 
-                        p[layer][i].weight[j] += 0.002 * p[layer][i].Sigma * (1- (p[layer][i].Output * p[layer][i].Output)) * input;
-                      
-                    }
-
-                     p[layer][i].BiasWeight += 0.002 * p[layer][i].Sigma * (1 - (p[layer][i].Output * p[layer][i].Output));
-
-                }
-        }
-
-        private void CalculateSigma(double _expectation)
-        {
-            for (int i = 0; i < p[totalLayersCount - 1].Length; i++)
-                p[totalLayersCount - 1][i].Sigma = _expectation - p[totalLayersCount - 1][i].Output;
 
 
-            for (int layer = totalLayersCount - 2; layer >= 0; layer--)
-                for (int i = 0; i < p[layer].Length; i++)
-                {
-                    double sigmaSum = 0;
-                    for (int j = 0; j < p[layer + 1].Length; j++)
-                        sigmaSum += p[layer + 1][j].weight[i] * p[layer + 1][j].Sigma;
 
-                    p[layer][i].Sigma = sigmaSum;
-                }
-        }
 
         private void buttonClear_Click(object sender, EventArgs e)
         {
-            InitTopology(inputsCount, outputsCount, totalLayersCount, perceptronsInHiddenLayer, rnd);
-            Points = new List<double[]>();
+            nn = new NeuralNetwork(9, 1, 3, 5);
             pictureBoxResult.Refresh();
         }
     }
--- NeuralNetwork_WinForms/Perceptron.cs
+++ NeuralNetwork_WinForms/Perceptron.cs
@@ -9,32 +9,151 @@ namespace NeuralNetwork_WinForms
 
     class Perceptron
     {
-        public double[] weight;
-        public double BiasWeight;
-        public double Output;
-        public double Sigma;
+        public float[] weight;
+        public float BiasWeight;
+        public float Output;
+        public float Sigma;
 
         public Perceptron(int _inputsCount, Random _rnd)
         {
-            weight = new double[_inputsCount];
+            weight = new float[_inputsCount];
             for (int i = 0; i < weight.Length; i++)
-                weight[i] = _rnd.NextDouble()-0.5;
+                weight[i] = (float)(_rnd.NextDouble()-0.5);
 
             BiasWeight =2;
         }
 
-        public double ShowMatrix(double[] _normalizedInputsMatrix)
+        public float ShowMatrix(float[] _normalizedInputsMatrix)
         {
-            double sum = BiasWeight;
+            float sum = BiasWeight;
             for (int i = 0; i < _normalizedInputsMatrix.Length; i++)
                 sum += (_normalizedInputsMatrix[i] * weight[i]);
 
-            Output = Math.Tanh(sum); //2 / (1 + Math.Exp(-sum)) - 1;
+            Output = (float)Math.Tanh(sum); //2 / (1 + Math.Exp(-sum)) - 1;
 
             return Output;
         }
 
 
+
+    }
+    class NeuralNetwork
+    {
+        public List<float[]> InputWithOneOutputMatrix;
+        int inputsCount;
+        int outputsCount;
+        int totalLayersCount;
+        int perceptronsInHiddenLayer;
+        float learningRate;
+        Random rnd;
+
+        Perceptron[][] p;
+
+
+        public NeuralNetwork(int _inputs, int _outputs, int _totalLayersCount, int _perceptronsInHiddenLayer)
+        {
+            inputsCount = _inputs;
+            outputsCount = _outputs;
+            totalLayersCount = _totalLayersCount;
+            perceptronsInHiddenLayer = _perceptronsInHiddenLayer;
+            rnd = new Random();
+            InputWithOneOutputMatrix = new List<float[]>();
+
+            InitTopology();          
+        }
+
+        private void InitTopology()
+        {
+            p = new Perceptron[totalLayersCount][];// 3 layers
+
+            p[0] = new Perceptron[inputsCount];//input layer
+            p[totalLayersCount - 1] = new Perceptron[outputsCount];//output layer 
+
+            for (int i = 1; i < totalLayersCount - 1; i++) //hidden layers
+                p[i] = new Perceptron[perceptronsInHiddenLayer];
+
+
+            //initialize weight and other stuff
+            for (int layer = 0; layer < p.Length; layer++)
+                for (int perceptron = 0; perceptron < p[layer].Length; perceptron++)
+                    if (layer - 1 < 0)
+                        p[layer][perceptron] = new Perceptron(2, rnd);
+                    else
+                        p[layer][perceptron] = new Perceptron(p[layer - 1].Length, rnd);
+        }
+
+        public float CalculateOutputs(float[] _point)
+        {
+            for (int i = 0; i < p[0].Length; i++)
+                p[0][i].ShowMatrix(_point);//show data input layers
+
+            for (int layers = 1; layers < totalLayersCount; layers++)  // show data rest layers
+                for (int i = 0; i < p[layers].Length; i++)
+                {
+                    float[] matrix = new float[p[layers - 1].Length];
+                    for (int j = 0; j < p[layers - 1].Length; j++)
+                        matrix[j] = p[layers - 1][j].Output;
+
+                    p[layers][i].ShowMatrix(matrix);
+                }
+
+            return p[totalLayersCount - 1][0].Output;
+        }
+
+        public  void UpgradeWeight(float[] _point)
+        {
+            for (int layer = 0; layer < totalLayersCount; layer++)
+                for (int i = 0; i < p[layer].Length; i++)
+                {
+                    float input = 0;
+                    float func = learningRate * p[layer][i].Sigma * (1 - (p[layer][i].Output * p[layer][i].Output));
+
+
+                    for (int j = 0; j < p[layer][i].weight.Length; j++)
+                    {
+
+                        if (layer == 0)
+                            input = _point[j];///pomylonailosc wejsc ziloscia neuronow wejsciowych
+                        else
+                            input = p[layer - 1][j].Output;
+
+                        p[layer][i].weight[j] += func * input;
+
+                    }
+
+                    p[layer][i].BiasWeight += func;
+
+                }
+        }
+
+        public void CalculateSigma(float _expectation)
+        {
+            for (int i = 0; i < p[totalLayersCount - 1].Length; i++)
+                p[totalLayersCount - 1][i].Sigma = _expectation - p[totalLayersCount - 1][i].Output;
+
+
+            for (int layer = totalLayersCount - 2; layer >= 0; layer--)
+                for (int i = 0; i < p[layer].Length; i++)
+                {
+                    float sigmaSum = 0;
+                    for (int j = 0; j < p[layer + 1].Length; j++)
+                        sigmaSum += p[layer + 1][j].weight[i] * p[layer + 1][j].Sigma;
+
+                    p[layer][i].Sigma = sigmaSum;
+                }
+        }
+
+        public void Learn(int epoch = 2000, double _learningRate = 0.02)
+        {
+            learningRate = (float)_learningRate;
+            for (int i = 0; i < epoch; i++)
+                foreach (float[] point in InputWithOneOutputMatrix)
+                {
+                    CalculateOutputs(new float[] { point[0], point[1] });
+                    CalculateSigma(point[2]);
+                    UpgradeWeight(point);
+                }
+        }
 
     }
 }
